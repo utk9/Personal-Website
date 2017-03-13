e6f636c4a0f0462f0ff985ebe57d828ed4aec7b6
utk9 <utkarshlamba.2@gmail.com>
2017-03-12 19:10:28 -0700

    Add corousel images

---
+++ v2/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.img-carousel').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: true,
+		prevArrow: false,
+		nextArrow: false
+	});
+});
